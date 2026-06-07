2e9314c0ac7e8305b0b580f6168550371654d90d
Saurabh Salvi <saurabh.salvi@haptiq.com>
2026-06-07 18:33:48 +0530

    Day 2 Coding Problems

---
+++ Day 2/3 Time Complexity Problems/2.py
@@ -0,0 +1,32 @@
+# An array A consisting of N different integers is given. The array contains integers in the range [1..(N + 1)], which means that exactly one element is missing.
+
+# Your goal is to find that missing element.
+
+# Write a function:
+
+# class Solution { public int solution(int[] A); }
+# content_copy
+
+# that, given an array A, returns the value of the missing element.
+
+# For example, given array A such that:
+
+#   A[0] = 2
+#   A[1] = 3
+#   A[2] = 1
+#   A[3] = 5
+
+# content_copy
+# the function should return 4, as it is the missing element.
+
+# Write an efficient algorithm for the following assumptions:
+
+# N is an integer within the range [0..100,000];
+# the elements of A are all distinct;
+# each element of array A is an integer within the range [1..(N + 1)].
+
+def solution(A):
+    n = len(A)
+    expected_sum = (n + 1) * (n + 2) // 2
+    actual_sum = sum(A)
+    return expected_sum - actual_sum
